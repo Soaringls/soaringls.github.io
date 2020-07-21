e08a93eb7be51726198a1b1f825ac25f87f71e94
Soaringls <upc_ls@163.com>
2020-07-21 15:55:54 +0800

    update note 2020.07.21 15:55:54

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,7 +9,7 @@ HEXO_DIR=$(cd $(dirname $0); pwd)/..
 # src filse
 cd ${HEXO_DIR}
 git add .
-commit_msg=$(date "+%Y-%m-%d %H:%M:%S")
+commit_msg=$(date "+%Y.%m.%d %H:%M:%S")
 git commit -m "update note $commit_msg"
 git push origin note
 
